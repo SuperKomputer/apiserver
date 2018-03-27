2a961f60c5e8f80978f13b71b97b2101fbfa308a
Fan Zhang 79010 <fanz@vmware.com>
2018-03-27 08:59:19 -0700

    Initial commit - Created api server - Added REST API and implementations
    TODO:
    - login
    - initilize server

---
+++ pkg/service/login.go
@@ -0,0 +1,8 @@
+package service
+
+import "net/http"
+
+// Login parse and decode the request body into a new `Credentials` instance
+func Login(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
